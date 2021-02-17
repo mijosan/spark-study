558c2846be6ca17491361b89902f95092203dd55
ChoiTaesan <mijosan@naver.com>
2021-02-17 23:21:46 +0900

    feat : page 123

--- beginning-spark-examples/src/test/java/com/wikibooks/spark/ch02/book/TransformationTest.java
+++ beginning-spark-examples/src/test/java/com/wikibooks/spark/ch02/book/TransformationTest.java
@@ -18,6 +18,8 @@ import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import scala.Tuple2;
+
 public class TransformationTest {
 
     private static SparkConf conf;
@@ -38,27 +40,134 @@ public class TransformationTest {
     }
 
     @Test
+    public void groupByKeyTest() {
+        // given
+        List<Tuple2<String, Integer>> data = Arrays.asList(new Tuple2("a", 1), new Tuple2("b", 1), new Tuple2("c", 1));
+        JavaPairRDD<String, Integer> rdd1 = sc.parallelizePairs(data);
+
+        // when
+        JavaPairRDD<String, Iterable<Integer>> rdd2 = rdd1.groupByKey();
+
+        // then
+        System.out.println(rdd2.collect());
+        assertThat(rdd2.collect().get(0)._1()).isEqualTo("a");
+        assertThat(rdd2.collect().get(1)._1()).isEqualTo("b");
+        assertThat(rdd2.collect().get(2)._1()).isEqualTo("c");
+    }
+
+    @Test
+    public void groupByTest() {
+        // given
+        JavaRDD<Integer> rdd1 = sc.parallelize(Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9, 10));
+
+        // when
+        JavaPairRDD<String, Iterable<Integer>> rdd2 = rdd1.groupBy(v -> (v % 2 == 0) ? "even" : "odd");
+
+        // then ? collect를 여러번 쓰면 여러번의 트랜스포메이션 연산이 일어나나 ?
+        System.out.println(rdd2.collect());
+        assertThat(rdd2.collect().get(0)._1()).isEqualTo("even");
+        assertThat(rdd2.collect().get(1)._1()).isEqualTo("odd");
+    }
+
+    // ? a1 b2 c 가 아니고 a b1 c2 인 이유..
+    @Test
+    public void zipPartitionTest() {
+        // given
+        JavaRDD<String> rdd1 = sc.parallelize(Arrays.asList("a", "b", "c"), 3);
+        JavaRDD<Integer> rdd2 = sc.parallelize(Arrays.asList(1, 2), 3);
+
+        // when 
+        JavaRDD<String> rdd3 = rdd1.zipPartitions(rdd2, (Iterator<String> t1, Iterator<Integer> t2) -> {
+            List<String> list = new ArrayList<>();
+            t1.forEachRemaining((String s) -> {
+                if (t2.hasNext()) {
+                    list.add(s + t2.next());
+                } else {
+                    list.add(s);
+                }
+            });
+            
+            return list.iterator();
+        });
+
+        // then
+        System.out.println(rdd3.collect());
+    }
+    @Test
+    public void zipTest() {
+        // given
+        JavaRDD<String> rdd1 = sc.parallelize(Arrays.asList("a", "b", "c"));
+        JavaRDD<Integer> rdd2 = sc.parallelize(Arrays.asList(1, 2, 3));
+
+        // when
+        JavaPairRDD<String, Integer> result = rdd1.zip(rdd2);
+        
+        // then
+        System.out.println(result.collect());
+    }
+
+    @Test
+    public void flatMapvalueTest() {
+        // given
+        List<Tuple2<Integer, String>> data = Arrays.asList(new Tuple2(1, "a,b"), new Tuple2(2, "a,c"), new Tuple2(1, "d,e"));
+
+        // when
+        JavaPairRDD<Integer, String> rdd1 = sc.parallelizePairs(data);
+        
+        JavaPairRDD<Integer, String> rdd2 = rdd1.flatMapValues(v -> Arrays.asList(v.split(",")).iterator());
+
+        // then
+        System.out.println(rdd2.collect());
+        assertThat(rdd2.collect().size()).isEqualTo(6);
+    }
+
+    @Test
+    public void mapValuesTest() {
+        // given
+        JavaRDD<String> rdd1 = sc.parallelize(Arrays.asList("a", "b", "c"));
+
+        // when
+        JavaPairRDD<String, Integer> rdd2 = rdd1.mapToPair(t -> new Tuple2<String, Integer>(t, 1))
+            .mapValues(v -> v + 1);
+        
+        // then
+        System.out.println(rdd2.collect());
+        assertThat(rdd2.collect().get(0)._2()).isEqualTo(2);
+        assertThat(rdd2.collect().get(1)._2()).isEqualTo(2);
+        assertThat(rdd2.collect().get(2)._2()).isEqualTo(2);
+    }
+
+    @Test
     public void mapPartitionsWithIndexTest() {
+        // given
         JavaRDD<Integer> rdd1 = sc.parallelize(Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9, 10), 3);
 
+        // when
         JavaRDD<Integer> rdd2 = rdd1.mapPartitionsWithIndex((Integer idx, Iterator<Integer> numbers) -> {
             List<Integer> result = new ArrayList<Integer>();
 
-                if (idx == 0) {
-                    numbers.forEachRemaining(i -> result.add(i + 1));
-                }
-                
-                return result.iterator();
-            }, true);
+            if (idx == 0) {
+                numbers.forEachRemaining(i -> result.add(i + 1));
+            }
+            
+            return result.iterator();
+        }, true);
 
-            assertThat(rdd2.collect().get(0)).isEqualTo(2);
+        // then
+        System.out.println(rdd2.collect());
+        assertThat(rdd2.collect().get(0)).isEqualTo(2);
+        assertThat(rdd2.collect().get(1)).isEqualTo(3);
+        assertThat(rdd2.collect().get(2)).isEqualTo(4);
     }
 
     // 각 파티션 요소에 대한 이터레이터를 전달받아 함수 내부에서 파티션의 개별 요소에 대한
     // 작업을 처리하고 그 결과를 다시 이터레이터 타입으로 되돌려줘야 합니다.
     @Test
     public void mapPartitionTest() {
+        // given
         JavaRDD<Integer> rdd = sc.parallelize(Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9, 10), 3); 
+
+        // when
         JavaRDD<Integer> rdd2 = rdd.mapPartitions(n -> {
             System.out.println("DB연결 !!!");
             List<Integer> result = new ArrayList<Integer>();
@@ -67,6 +176,7 @@ public class TransformationTest {
             return result.iterator();
         });
 
+        // then
         assertThat(rdd2.collect().get(0)).isEqualTo(2);
     }
 
